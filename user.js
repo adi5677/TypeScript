dd8bb54ab3a4ad689cb8a01d84b191abc9c512e6
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-07 14:37:11 +0530

    Refactor user.js and user.ts to include type error comments and variable assignments

---
+++ user.js
@@ -0,0 +1,8 @@
+// var age = 22;
+// age = "Aditya"; // This will cause a type error in TypeScript
+// console.log(age); // This line will not execute due to the type error
+
+
+
+let name = "Aditya";
+let name = "Prince";
---
+++ user.ts
@@ -0,0 +1,12 @@
+
+var age:number = 20;
+
+var s = "Aditya";
+s = "Prince";
+s = true; // This will cause a type error in TypeScript  
+
+
+//
+//
+
+// age = "Aditya"; // This will cause a type error in TypeScript
