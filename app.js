dff9f3e9f731ced580a43471fa0b7b79591193c7
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-07 15:08:38 +0530

    Add TypeScript 5.9.2 and user HTML template
    - Created package.json to include TypeScript as a dev dependency.
    - Added package-lock.json to lock TypeScript version at 5.9.2.
    - Introduced typescript/package.json for TypeScript configuration and metadata.
    - Added user.html with a basic structure for displaying user information.

---
+++ app.js
@@ -0,0 +1,8 @@
+var userName = "Aditya Sharma";
+var age = 22;
+var isMarried = false;
+var college = "HBS Noida";
+console.log(userName);
+console.log(age);
+console.log(isMarried);
+console.log(college);
---
+++ app.ts
@@ -0,0 +1,13 @@
+var userName:string = "Aditya Sharma";
+var age:number = 22;
+var isMarried:boolean = false; 
+var college:string = "HBS Noida";
+
+
+console.log(userName);
+console.log(age);
+console.log(isMarried);
+console.log(college);
+
+
+
