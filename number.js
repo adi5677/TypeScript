a58a3ee7a8f21a361fb964035f14b92b6262cad7
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 13:04:19 +0530

    Add number.js and number.ts files with examples of number operations and types

---
+++ number.js
@@ -0,0 +1,10 @@
+// This file is part of the TypeScript project; see https://www.typescriptlang.org/ for more information.
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var total = num1 + num2 + num3;
+console.log("Total:", total);
+var oct = 32769;
+var hexa = 0x0001;
+var binary = 1;
+console.log(oct + 10);
---
+++ number.ts
@@ -0,0 +1,13 @@
+// This file is part of the TypeScript project; see https://www.typescriptlang.org/ for more information.
+// var num1:number = 10
+// var num2:number = 20
+// var num3:number = 30
+
+// var total = num1 + num2 + num3;
+// console.log("Total:", total);
+
+var oct:number = 0o100001;
+var hexa:number = 0x0001;
+var binary:number = 0b0001;
+
+console.log(oct+10);
