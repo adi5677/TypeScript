f87efecba71fa9aacfe81496db336b35cea94e30
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 15:37:22 +0530

    Update bigint.js to use BigInt literals for improved precision

--- bigint.js
+++
@@ -1,6 +0,0 @@
-// console.log("bigint example");
-var bignum = 1234567890123456789012345678901234567890n;
-var x = 1n;
-var y = 2n;
-console.log(bignum + x);
-console.log(bignum + y);
--- output/bigint.js
+++ output/bigint.js
@@ -1,6 +1,6 @@
 // console.log("bigint example");
-var bignum = 1234567890123456789012345678901234567890;
-var x = 1;
-var y = 2;
+var bignum = 1234567890123456789012345678901234567890n;
+var x = 1n;
+var y = 2n;
 console.log(bignum + x);
 console.log(bignum + y);
