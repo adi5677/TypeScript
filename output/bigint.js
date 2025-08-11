c4ad3a6fd07d43ea3f73c2adca075613340e8fd3
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 16:06:56 +0530

    Add TypeScript declaration and source map files for bigint and symbol

--- output/bigint.js
+++ output/bigint.js
@@ -1,6 +1,9 @@
+"use strict";
 // console.log("bigint example");
+Object.defineProperty(exports, "__esModule", { value: true });
 var bignum = 1234567890123456789012345678901234567890n;
 var x = 1n;
 var y = 2n;
 console.log(bignum + x);
 console.log(bignum + y);
+//# sourceMappingURL=bigint.js.map
---
+++ output/symbol.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sym = Symbol();
+var sym2 = Symbol();
+var sym3 = Symbol('adi');
+var sym4 = Symbol('sharma');
+// console.log(sym == sym2); // false
+// console.log(sym);
+// console.log(sym2);
+console.log(sym == sym2); // false
+console.log(sym3 == sym4); // false
+console.log(sym == sym3); // false
+console.log(sym2 == sym4); // false
+console.log(sym);
+console.log(sym2);
+console.log(sym3);
+console.log(sym4);
+const id = Symbol('id');
+const obj = {
+    id: 10,
+    name: 'adi'
+};
+console.log(obj.id);
+//# sourceMappingURL=symbol.js.map
