9c869f8d7cd75509b10301204e950fe1820cb21b
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 18:15:33 +0530

    Add input.html and input.js files for user registration form

---
+++ output/input.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getInfo() {
+    const nameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    // console.log(nameInput.value, emailInput.value, passwordInput.value);
+}
+//# sourceMappingURL=input.js.map
