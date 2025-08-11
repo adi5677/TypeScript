3de3327509f95e27a0510bb6d2c7c38b8ba5831a
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 14:13:01 +0530

    Add null.js and null.ts files with examples of null and undefined types

---
+++ null.js
@@ -0,0 +1,14 @@
+// var data:null = null;
+// var item = "" //as null;
+// var val:undefined = undefined; // as undefined
+var data = null;
+// data = "Hello";
+var userName = null;
+var login = true;
+if (login) {
+    userName = "Aditya Sharma";
+}
+else {
+    userName = "Guest";
+}
+console.log(typeof data);
