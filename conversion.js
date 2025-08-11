72b4486eb8e7db1aa2fc4c3ab9ce647aec0ffda5
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 13:04:03 +0530

    Add conversion.js and conversion.ts files with number conversion examples

---
+++ conversion.js
@@ -0,0 +1,5 @@
+var item = 100;
+var item2 = "200";
+var item2converted = Number(item2);
+console.log(item + item2converted); // 300
+console.log(item + parseInt(item2)); // 300
---
+++ conversion.ts
@@ -0,0 +1,14 @@
+var item:number = 100;
+var item2 = "200";
+
+var item2converted = Number(item2)
+console.log(item + item2converted); // 300
+
+console.log(item + parseInt(item2)); // 300
+
+// var data = true;
+// data = 40
+// data = "Hello"
+
+var data: number | string = 30
+data = "anil"
