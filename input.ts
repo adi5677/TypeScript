ae01020659761564125b3e34f8c476374e91d28b
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 18:15:58 +0530

    Add input.ts, input.d.ts, input.d.ts.map, and input.js.map files for user input handling

---
+++ input.ts
@@ -0,0 +1,6 @@
+function getInfo() {
+    const nameInput = document.getElementById('username') as HTMLInputElement;
+    const emailInput = document.getElementById('email') as HTMLInputElement;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    // console.log(nameInput.value, emailInput.value, passwordInput.value);
+}
