b9ac01d1d8a0d1ebc593f0eb34d58d6d32a5bb0d
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 15:32:49 +0530

    Add bigint examples in JavaScript and TypeScript files

---
+++ bigint.ts
@@ -0,0 +1,8 @@
+// console.log("bigint example");
+
+var bignum: bigint = 1234567890123456789012345678901234567890n;
+var x = 1n;
+var y = 2n;
+
+console.log(bignum + x);
+console.log(bignum + y);
---
+++ output/bigint.js
@@ -0,0 +1,6 @@
+// console.log("bigint example");
+var bignum = 1234567890123456789012345678901234567890;
+var x = 1;
+var y = 2;
+console.log(bignum + x);
+console.log(bignum + y);
