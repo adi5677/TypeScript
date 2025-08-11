b1111fc8aabe8db0fa546cc7f91d5e0a19465e86
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-11 13:50:42 +0530

    Add string.js and string.ts files with examples of string operations and type annotations

---
+++ string.js
@@ -0,0 +1,8 @@
+// var str:string = "Aditya"
+var str = "hello how are you";
+var str2 = 'hello how are you';
+var str3 = "hello how are you";
+var age = 22;
+var userName = "Aditya Sharma";
+var info = "Name: ".concat(userName, ", Age: ").concat(age, " years");
+console.log(info);
---
+++ string.ts
@@ -0,0 +1,23 @@
+// var str:string = "Aditya"
+var str:string = "hello how are you"
+var str2:string = 'hello how are you'
+var str3:string = `hello how are you`
+
+var age : number = 22;
+var userName: string = "Aditya Sharma";
+
+var info: string = `Name: ${userName}, Age: ${age} years` ;
+console.log(info);
+
+var num:number = 100;
+// var data:string = num.toString();  //1st way
+
+// var data:string = " " + num;  //2nd way
+// console.log(data);
+
+var boolData = true;
+var data:string = boolData.toString();
+
+var item:boolean = true;
+
+var info1 = "Aditya Sharma is " + (item ? "available" : "not available");
