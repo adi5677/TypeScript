cdb27df9206b6c914e95d018d2ff5913a0b46327
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-12 17:26:30 +0530

    Object in TypeScript

---
+++ object.ts
@@ -0,0 +1,21 @@
+// var userData = {
+//     name: 'Aditya Sharma',
+//     age: 30,
+//     city: 'Noida',
+//     isAdmin: false
+// }
+
+var userData: {
+    name: string;
+    age: number;
+    city: string;
+    isAdmin: boolean;
+}={
+    name: 'Aditya Sharma',
+    age: 30,
+    city: 'Noida',
+    isAdmin: false
+}
+
+userData.name = 'Prince Sharma';
+console.log(userData);
