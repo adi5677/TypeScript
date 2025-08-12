372d1f620ffe36bba5e5f398d079c3a124fa9366
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-12 17:25:34 +0530

    updated in obj file

---
+++ output/object.js
@@ -0,0 +1,17 @@
+"use strict";
+// var userData = {
+//     name: 'Aditya Sharma',
+//     age: 30,
+//     city: 'Noida',
+//     isAdmin: false
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+var userData = {
+    name: 'Aditya Sharma',
+    age: 30,
+    city: 'Noida',
+    isAdmin: false
+};
+userData.name = 'Prince Sharma';
+console.log(userData);
+//# sourceMappingURL=object.js.map
