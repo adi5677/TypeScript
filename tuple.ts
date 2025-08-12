cde8bdcccfb61e457b776655b2ada825e6c5e599
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-12 16:24:55 +0530

    Add tuple.ts file with employee names and tuple data structure

---
+++ tuple.ts
@@ -0,0 +1,6 @@
+var emp:string[] = [ 'aditya', 'prince', 'golu'];
+var empData:[number,string,boolean] = [1,'aditya',true];
+
+// empData.push(2, 'prince', false); // This will cause an error since the tuple has a fixed structure
+console.log(emp);
+console.log(empData);
