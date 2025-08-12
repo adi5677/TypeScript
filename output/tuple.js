fa26c27ff4d432435f8e5114f46c195821e209ab
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-12 16:24:43 +0530

    Add tuple.ts, tuple.d.ts, tuple.js, and tuple.js.map files for tuple handling

---
+++ output/tuple.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var emp = ['aditya', 'prince', 'golu'];
+var empData = [1, 'aditya', true];
+// empData.push(2, 'prince', false); // This will cause an error since the tuple has a fixed structure
+console.log(emp);
+console.log(empData);
+//# sourceMappingURL=tuple.js.map
