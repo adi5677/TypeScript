c817d8f00d62e06882f4835e44f8050392939ec6
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-13 15:19:17 +0530

    Functions in TypeScript

---
+++ function.ts
@@ -0,0 +1,16 @@
+function fruits(): string {
+    return "Apple";
+}
+
+function simple(): string {
+    return "Hello";
+}
+
+function complex(): any {
+    return {
+        name: "Aditya",
+        age: 25,
+        isActive: true
+    };
+}
+
