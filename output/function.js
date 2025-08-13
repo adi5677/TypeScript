046ab065719ae6e59e5d0434721d801d585f2653
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-13 15:18:45 +0530

    Function file supporot

---
+++ output/function.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function fruits() {
+    return "Apple";
+}
+function simple() {
+    return "Hello";
+}
+function complex() {
+    return {
+        name: "Aditya",
+        age: 25,
+        isActive: true
+    };
+}
+//# sourceMappingURL=function.js.map
