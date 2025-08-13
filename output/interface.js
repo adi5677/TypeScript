ded3bea06d4ff8150262b5f7213f2ef5a91a6f9e
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-13 16:00:23 +0530

    Interface Support File

---
+++ output/interface.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var studentObj = {
+    name: "Adi",
+    age: 25,
+    isEnrolled: true
+};
+var teacherObj = {
+    name: "Mr. Sachin",
+    age: 40,
+    isEnrolled: false,
+    subject: "Mathematics"
+};
+var managerObj = {
+    name: "Mr. Sagar",
+    age: 35,
+    isEnrolled: true
+};
+console.log(studentObj);
+console.log(teacherObj);
+console.log(managerObj);
+//# sourceMappingURL=interface.js.map
