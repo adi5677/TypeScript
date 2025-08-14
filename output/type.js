9a2b9ddac43a6212dee0ba8cea8879490c6ea2fa
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-14 14:30:27 +0530

    Support File Type

---
+++ output/type.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var empData = {
+    name: "Prince",
+    age: 20,
+    email: "prince@example.com"
+};
+var stuData = {
+    name: "raju",
+    age: 18,
+    email: "raju@example.com"
+};
+//# sourceMappingURL=type.js.map
