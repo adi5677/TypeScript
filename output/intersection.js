cba926e2f37448c8c8ce3a111343316712d3b0ee
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-14 14:23:31 +0530

    Support File Intersection

---
+++ output/intersection.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// type x = string | number | boolean;
+var PersonDataA = {
+    name: "adi",
+    age: 30,
+    email: "adi@example.com"
+};
+var PersonDataB = {
+    name: "Golu",
+    age: 30,
+    email: "golu@example.com"
+};
+console.log(PersonDataA);
+console.log(PersonDataB);
+//# sourceMappingURL=intersection.js.map
