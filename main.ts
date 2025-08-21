967dd0cbad4f85ba480e34ea99c8148d40078d4f
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-21 12:24:31 +0530

    Main file in TS

---
+++ main.ts
@@ -0,0 +1,10 @@
+import type UserInfoType = require("./type/type");
+
+var userInfo:UserInfoType = {
+    name: 'Aditya',
+    age:30,
+    email:'adis6469@gmail.com',
+    password:'5677Adi#'
+}
+
+console.log(userInfo);
