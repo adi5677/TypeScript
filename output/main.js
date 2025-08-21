b7d25c542ee8f507e2e853c539a98c1a6c320831
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-08-21 12:15:30 +0530

    Main and Type Support file

---
+++ output/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var userInfo = {
+    name: 'Aditya',
+    age: 30,
+    email: 'adis6469@gmail.com',
+    password: '5677Adi#'
+};
+console.log(userInfo);
+//# sourceMappingURL=main.js.map
